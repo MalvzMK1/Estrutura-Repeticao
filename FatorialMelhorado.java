557785dc9aac7b3401d79437877be07d296183a9
MATHEUS ALVES DE OLIVEIRA <matheus.adeoliveira11@gmail.com>
2022-03-30 11:20:34 -0300

    Em andamento

---
+++ FatorialMelhorado.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class FatorialMelhorado {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            while (true) {
+                System.out.print("Informe um numero positivo: ");
+                long n = sc.nextInt();
+                long fat = n;
+                if (n == 0) {
+                        System.out.println(n + "! = 1");
+                        System.exit(0);
+                    }
+                if (n >= 0) {
+                    for (int i = 2 ; i < n ; i++) {
+                        fat *= i;
+                    }
+                    System.out.println(n + "! = " + fat);
+                    System.exit(0);
+                }
+                System.out.println("Somente numeros positivos");
+            }
+            System.out.println("Deseja continuar? Sim (1) ou nao (0)?");
+            int sn = sc.nextInt();
+            if (sn == 1) {
+                
+            }
+            else {
+                break;
+            }
+        }
+    }
+}
