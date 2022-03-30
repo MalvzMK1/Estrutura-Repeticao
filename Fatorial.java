2cdb659d9ae20243185a20e01ccfc03d1964c97c
MATHEUS ALVES DE OLIVEIRA <matheus.adeoliveira11@gmail.com>
2022-03-30 08:27:48 -0300

    otimização

--- Fatorial.java
+++ Fatorial.java
@@ -1,10 +1,11 @@
 class Fatorial {
     public static void main(String args[]) {
-        long num = 5;
-
-        for (long i = num ; i >= 1 ; i=i-1) {
-            num = num * i;
+        long num = 15;
+        long fat = num;
+        
+        for (long i = num-1 ; i >= 2 ; i=i-1) {
+            fat = fat * i;
         }
-        System.out.println(num);
+        System.out.println(num + "! = " + fat);
     }
 }
