4e4f0d3acdd555538ff806493b6ec85924477b77
MATHEUS ALVES DE OLIVEIRA <matheus.adeoliveira11@gmail.com>
2022-04-06 10:20:23 -0300

    potencia

--- Potencia.java
+++ Potencia.java
@@ -4,6 +4,22 @@ class Potencia {
     public static void main(String args[]) {
         Scanner ler = new Scanner(System.in);
 
+        System.out.print("Base: ");
+        int base = ler.nextInt();
+        System.out.print("Expoente: ");
+        int expoente = ler.nextInt();
+        int contador = base;
         
+        // Base * base o tanto de vezes que o valor da potencia determina
+        if (expoente <= 1) {
+            System.out.println("1");
+        }
+        else {
+            while (expoente != 1) {
+                contador = base * contador;
+                expoente--; 
+            }
+            System.out.println(contador);
+        }
     }
 }
