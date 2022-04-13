7dd28ea25f536d4f016b30932483dab18af2478b
MATHEUS ALVES DE OLIVEIRA <matheus.adeoliveira11@gmail.com>
2022-04-13 08:27:45 -0300

    melhora no código

--- Potencia.java
+++ Potencia.java
@@ -4,22 +4,35 @@ class Potencia {
     public static void main(String args[]) {
         Scanner ler = new Scanner(System.in);
 
-        System.out.print("Base: ");
-        int base = ler.nextInt();
-        System.out.print("Expoente: ");
-        int expoente = ler.nextInt();
-        int contador = base;
-        
-        // Base * base o tanto de vezes que o valor da potencia determina
-        if (expoente <= 1) {
-            System.out.println("1");
-        }
-        else {
-            while (expoente != 1) {
-                contador = base * contador;
-                expoente--; 
+        int base = 0;
+        int expo = 0;
+
+        while (true) {
+            while (true) {
+                System.out.print("\nInsira o valor da base: ");
+                base = ler.nextInt();
+                System.out.print("Insira o valor do expoente: ");
+                expo = ler.nextInt();
+                if (base >= 0 && expo >= 0) {
+                    break;
+                }
             }
-            System.out.println(contador);
+            long pot = base;
+
+            if (expo == 0) {
+                System.out.println("\n" + base + " ^ " + expo + " = 1");
+                break;
+            }
+
+            for (int i=1 ; i<expo ; i++) {
+                pot = pot * base;
+            }
+            System.out.println("\n" + base + " ^ " + expo + " = " + pot);
+            System.out.println("\nDeseja continuar? [s/n]");
+            char op = ler.next().charAt(0);
+            if (op != 's' && op != 'y') {
+                System.exit(0);
+            }
         }
     }
 }
